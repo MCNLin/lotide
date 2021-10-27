c77e1984f6a699202980464fba1648242ee00fe3
Mei Nghiem-Lin <mcnghiem@gmail.com>
2021-10-27 02:10:45 +0000

    add function to compare arrays true/false

---
+++ eqArrays.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`🛑️ 🛑️ Assertion failed 🛑️ 🛑️: ${actual} !== ${expected}`);
+  } else {
+    console.log(`✅️ ✅️Assertion passed ✅️ ✅️ : ${actual} === ${expected}`);
+  }
+
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)
+
+//function eqArrays(a,b) { return !!a && !!b && !(a<b || b<a); }
+function eqArrays(a, b) {
+  if(a.length !== b.length) {
+    return false;
+  } for (var i = 0; i < a.length; ++i) {
+    if (a[i] !== b[i]) {
+      return false;
+    }
+  }  
+  return true;
+};
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]))
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)
