f1f0db92bd24845114cd37885353a260083bf456
Mei Nghiem-Lin <mcnghiem@gmail.com>
2021-10-27 02:31:30 +0000

    use eslint

--- eqArrays.js
+++ eqArrays.js
@@ -8,19 +8,19 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(a, b) {
-  if(a.length !== b.length) {
+  if (a.length !== b.length) {
     return false;
-  } for (var i = 0; i < a.length; ++i) {
+  } for (let i = 0; i < a.length; i++) {
     if (a[i] !== b[i]) {
       return false;
     }
-  }  
+  }
   return true;
 };
 console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
+console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
 
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]))
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]));
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
