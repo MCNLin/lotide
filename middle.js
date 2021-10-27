345b6592e052c18053046e7a88a0412c33300e18
Mei Nghiem-Lin <mcnghiem@gmail.com>
2021-10-27 23:04:02 +0000

    change error reference

--- middle.js
+++ middle.js
@@ -1,5 +1,4 @@
 const eqArrays = function(a, b) {
-  console.log('first param:', a, 'second param', b);
   if (a.length !== b.length) {
     return false;
   } for (let i = 0; i < a.length; i++) {
@@ -9,9 +8,9 @@ const eqArrays = function(a, b) {
   }
   return true;
 };
+
 const assertArrayEquals = function(a ,b) {
   if (eqArrays(a, b) === true) {
-    
     console.log(`🌟️ 🌟️ Assertion Passed 🌟️ 🌟️: ${a} is equal to ${b}`);
   } else {
     console.log(`🚫️ 🚫️ Assertion Failed 🚫️ 🚫️: ${a} is not equal to ${b}`);
@@ -28,9 +27,9 @@ const middle = (array) => {
     return [array[Math.floor(array.length / 2)]];
   }
 };
-assertArrayEquals(middle([1])); // => []
-assertArrayEquals(middle([1, 2])); // => []
-assertArrayEquals(middle([1, 2, 3])); // => [2]
-assertArrayEquals(middle([1, 2, 3, 4, 5])); // => [3]
-assertArrayEquals(middle([1, 2, 3, 4])); // => [2, 3]
-assertArrayEquals(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
+assertArrayEquals(middle([1]), []);
+assertArrayEquals(middle([1,2]), []);
+assertArrayEquals(middle([1,2,3]), [2]);
+assertArrayEquals(middle([1,2,3,4,5]), [3]);
+assertArrayEquals(middle([1,2,3,4]), [2,3]);
+assertArrayEquals(middle([1,2,3,4,5,6]), [3,4]);
