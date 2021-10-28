7af778f45d6056042ee367022bf60104cac2bb63
Mei Nghiem-Lin <mcnghiem@gmail.com>
2021-10-28 02:05:17 +0000

    create letterPosition function

---
+++ letterPositions.js
@@ -0,0 +1,37 @@
+const eqArrays = function(a, b) {
+  if (a.length !== b.length) {
+    return false;
+  } for (let i = 0; i < a.length; i++) {
+    if (a[i] !== b[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+const assertArrayEquals = function(a ,b) {
+  if (eqArrays(a, b) === true) {
+    console.log(`🌟️ 🌟️ Assertion Passed 🌟️ 🌟️: ${a} is equal to ${b}`);
+  } else {
+    console.log(`🚫️ 🚫️ Assertion Failed 🚫️ 🚫️: ${a} is not equal to ${b}`);
+  }
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) { //loop through array
+    if ((sentence[i] !== " ")) {
+      if (results[sentence[i]]) {
+        results[sentence[i]].push(i);
+      } else {
+        results[sentence[i]] = [i];
+      }
+    }
+  
+
+  }
+  return results;
+};
+// Test Code:
+
+console.log(letterPositions("how are you"));
+assertArrayEquals(letterPositions("hello").e, [1]);
