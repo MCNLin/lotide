f834b5f515f589f5444b706adf102406f4bdc46a
Mei Nghiem-Lin <mcnghiem@gmail.com>
2021-11-01 23:06:29 +0000

    mocha & chai tailTest.js

--- test/tailsTest.js
+++ test/tailsTest.js
@@ -1,18 +1,23 @@
-const assertEqual = require('../assertEqual');
+// const assertEqual = require('../assertEqual');
+// const tail = require('../tail');
+
+const assert = require('chai').assert;
 const tail = require('../tail');
 
+describe("#tail", () => {
+  it("returns [2, 3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+  it("returns [] for [1]", () => {
+    assert.deepEqual(tail([1]), []);
+  });
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
+  it("returns [6, 7, 8] for [5 ,6 , 7, 8]", () => {
+    assert.deepEqual(tail([5, 6, 7, 8]),[6, 7, 8]);
+  });
+  
+});
 
-assertEqual(result.length, 2);// return true
-assertEqual(result[0],'Lighthouse');//return true
-assertEqual(result[1],'Labs');  //return true
 
-console.log(tail([1]));
-console.log(tail([]));
+
