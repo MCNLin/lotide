df27a6104b7ab227f5ae1c788784ba720e54eace
Mei Nghiem-Lin <mcnghiem@gmail.com>
2021-11-01 18:17:31 +0000

    refactor and modularize head

--- head.js
+++ head.js
@@ -1,10 +1,11 @@
 const assertEqual = require('./assertEqual');
 //create a head function which returns the 1st item in the array
+
 const head = function(array){
   return array[0];
 }
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+module.exports = head;
 
 
   
